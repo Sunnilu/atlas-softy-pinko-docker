2a6bc333d06f637187fc34a7967a6728a9790669
Suntha <Suntha.Lucas@atlasschool.com>
2024-06-26 13:54:11 -0500

    added COR to api

--- task3/api.py
+++ task3/api.py
@@ -1,10 +1,12 @@
-from flask import Flask # type: ignore
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/hello')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5252)
+    app.run(host='0.0.0.0', port=5252
