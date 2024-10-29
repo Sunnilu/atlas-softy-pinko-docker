92b465f292e9029277a3f31a905da33cc3b3fbcb
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-29 16:28:39 -0500

    api task 3

---
+++ task3/back-end/api.py
@@ -0,0 +1,13 @@
+import __main__
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.rute('/api/hello')
+def hello_world():
+    return 'Hello, World'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5252)
